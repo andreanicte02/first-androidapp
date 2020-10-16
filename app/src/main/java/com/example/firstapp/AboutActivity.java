0d120900865c417a02fbc8072d0c591d79e95b26
andreanicte02 <andreanicte02@gmail.com>
2020-10-16 16:12:00 -0600

    -button back

--- app/src/main/java/com/example/firstapp/AboutActivity.java
+++ app/src/main/java/com/example/firstapp/AboutActivity.java
@@ -3,12 +3,26 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        button = findViewById(R.id.buttonBackGoMain);
+
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //terminar una activity
+                finish();
+            }
+        };
     }
 }
