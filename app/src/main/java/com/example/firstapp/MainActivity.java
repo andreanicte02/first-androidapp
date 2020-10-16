aceeb135a96eb85d70aff3cac6a05494333f6282
andreanicte02 <andreanicte02@gmail.com>
2020-10-15 19:46:19 -0600

    -probando TOAST

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -57,7 +58,8 @@ public class MainActivity extends AppCompatActivity {
         AdapterView.OnItemClickListener itemClikc = new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                textResult.setText(opciones.get(position) + "hola");
+                
+                Toast.makeText(MainActivity.this, opciones.get(position), Toast.LENGTH_SHORT).show();
             }
         };
 
